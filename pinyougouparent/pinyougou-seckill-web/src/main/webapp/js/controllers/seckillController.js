2602aa670a5f49a189db7af65866581213fa56ba
xiaoz <xiaozegavin@outlook.com>
2020-03-12 10:05:30 +0800

    逆向工程添加

---
+++ pinyougouparent/pinyougou-seckill-web/src/main/webapp/js/controllers/seckillController.js
@@ -0,0 +1,5 @@
+app.controller('seckillController', function ($scope,$location,seckillService) {
+
+
+
+});
---
+++ pinyougouparent/pinyougou-seckill-web/src/main/webapp/js/services/seckillService.js
@@ -0,0 +1,11 @@
+app.service('seckillService', function ($http) {
+
+    this.createNative = function () {
+        return $http.get("./weixinPay/createNative.do");
+    };
+
+    this.queryPayStatus = function (out_trade_no) {
+        return $http.get("./weixinPay/queryPayStatus.do?out_trade_no=" + out_trade_no);
+    };
+
+});
